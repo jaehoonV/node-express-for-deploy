180fc86eaf76f1e6ad77437f3019ecdca92040c1
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-01-04 09:12:08 +0900

    DB Connection 분리

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql');  // mysql 모듈 로드
 
-const conn = {  // mysql 접속 설정
-  host: 'svc.gksl2.cloudtype.app',
-  port: '32059',
-  user: 'root',
-  password: '1032',
-  database: 'color_memo'
-};
-
-let connection = mysql.createConnection(conn); // DB 커넥션 생성
-connection.connect();   // DB 접속
+// mariaDB Connection
+const maria = require('../ext/conn_mariaDB');
+maria.connect();   // DB 접속
 
 let sql = "SELECT EMAIL, USERNAME FROM `MEMBER`";
 var sql_res = [];
 var sql_data;
-connection.query(sql, function (err, results) {
+maria.query(sql, function (err, results) {
   if (err) {
       console.log(err);
   }
@@ -33,4 +25,4 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-connection.end(); // DB 접속 종료
+maria.end(); // DB 접속 종료
