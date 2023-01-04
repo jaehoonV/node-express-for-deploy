42d7456b2bf41a48825be509d598da52ff329dc6
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-01-04 19:47:11 +0900

    script, css 적용

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,8 @@ maria.query(sql, function (err, results) {
   console.log(sql_res);
 });
 
+router.use(express.static("public"));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', sql_data);
