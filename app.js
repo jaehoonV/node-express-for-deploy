3e90acb740a1a38b08ad1332e1e5f81372371f42
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-10-27 07:59:15 +0900

    Apply favicon

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const favicon = require('serve-favicon');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -19,6 +20,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.use(favicon(path.join(__dirname, 'public',  'favicon.ico')));
 // app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 app.use(express.static("views"));
